633f986009a7b74cf43eac6a5c4edc8fb477cae3
tom8u4286 <tom8u4286@gmail.com>
2020-03-04 17:15:52 +0800

    get_def.py added. skPublish.py is the api interface.

--- get_def.py
+++ get_def.py
@@ -1,5 +1,41 @@
-f = open("HouseOfCardsCh1Voc.txt")
-voc = f.read().split('\n')
+from skPublish import API
+import json
+import sys
+import xml.etree.ElementTree as ET
 
-for w in voc:
-    print(w)
+
+baseUrl = "https://dictionary.cambridge.org/api/v1/"
+dictCode = "english-chinese-traditional"
+accessKey = "L7E5Rwg0bImD3ZJNFH13CvVNpWnACp3vEpmP6QMOqyMDJKGQSLCZvQzMnHSrAv4I"
+api = API( baseUrl , accessKey)
+
+f = open("HouseOfCardsCh1Voc.txt")
+fw = open('HouseOFCardsCH1Voc_Chinese.txt','w')
+words = f.read().split('\n')
+final_l = []
+cnt = 0
+for word in words:
+    cnt+=1
+    try:
+        results = json.loads(api.searchFirst(dictCode, word , "xml"))
+        xmlcontent = results["entryContent"]
+    except:
+        print("line "+str(cnt)+", error occurred.")
+        continue
+    root = ET.fromstring(xmlcontent)
+    #word = root.find('header').find('title').text
+    pos = "("+root.find('toc-page').find('toc-entry').find('toc-pos-block').find('toc-pos').text[:3:]+")"
+
+    l = []
+    for i,item in enumerate(root.iter(tag='definition')):
+        l.append(str(i+1)+". "+item.find('trans').text)
+    
+    s = word + pos + " ".join(l)
+    #final_l.append(s)
+    fw.write(s+'\n')
+
+    sys.stdout.write("\rStatus:%s/%s"%(cnt, len(words)))
+    sys.stdout.flush()
+
+
+
